c2ad07f34eaed1cf7637cb60963947532e7bd1d6
高烽 <feng.gao@iscs.com.cn>
2017-05-18 15:40:23 +0800

    add Kotlin code.

---
+++ app/src/main/java/com/example/fenggao/myandroidframework/sample/Person.kt
@@ -0,0 +1,12 @@
+package com.example.fenggao.myandroidframework.sample
+
+/**
+ * Created by feng.gao on 2017/5/18.
+ */
+
+class Person(name: String, surname: String){
+    init {
+
+    }
+}
+
---
+++ app/src/main/java/com/example/fenggao/myandroidframework/sample/ProfileActivity.kt
@@ -0,0 +1,25 @@
+package com.example.fenggao.myandroidframework.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+import com.example.fenggao.myandroidframework.R
+import com.example.fenggao.myandroidframework.core.BaseActivity
+import com.example.fenggao.myandroidframework.core.CustomApplication
+import kotlinx.android.synthetic.main.activity_profile.*
+
+/**
+ * Created by feng.gao on 2017/5/17.
+ */
+
+class ProfileActivity : BaseActivity() {
+    override fun setUpData() {
+        super.setUpData()
+        setContentView(R.layout.activity_profile)
+        tv.text = CustomApplication.mTestNullPointers.toString()
+
+
+    }
+
+}
